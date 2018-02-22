099fbb909926e34528a6023b2eb7034fa04f662a
SpinnelSun <m.hollygard@gmail.com>
2018-02-22 18:41:15 -0300

    Inserção do EasyAccept na Facade

--- src/controllers/Facade.java
+++ src/controllers/Facade.java
@@ -1,48 +1,58 @@
 package controllers;
 
+import easyaccept.EasyAccept;
+
 public class Facade {
 	
+	private Sistema sistema;
+	
+	public static void main(String[] args) {
+		args = new String[] {"controllers.Facade", "acc-tst/us1_test.txt", "acc-tst/us2_test.txt"};
+		
+		EasyAccept.main(args);
+	}
+	
 	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
-		return;
+		this.sistema.cadastrarAluno(nome, matricula, codigoCurso, telefone, email);
 	}
 	
 	public String recuperaAluno(String matricula) {
-		return "";
+		return this.sistema.recuperaAluno(matricula);
 	}
 	
 	public String listarAlunos() {
-		return "";
+		return this.sistema.listarAlunos();
 	}
 	
 	public String getInfoAluno(String matricula, String atributo) {
-		return "";
+		return this.sistema.getInfoAluno(matricula, atributo);
 	}
 	
 	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
-		return;
+		this.sistema.tornarTutor(matricula, disciplina, proficiencia);
 	}
 	
 	public String recuperaTutor(String matricula) {
-		return "";
+		return this.sistema.recuperaTutor(matricula);
 	}
 	
 	public String listarTutores() {
-		return "";
+		return this.sistema.listarTutores();
 	}
 	
 	public void cadastrarHorario(String email, String horario, String dia) {
-		return;
+		this.cadastrarHorario(email, horario, dia);
 	}
 	
 	public void cadastrarLocalDeAtendimento(String email, String local) {
-		return;
+		this.cadastrarLocalDeAtendimento(email, local);
 	}
 	
 	public boolean consultaHorario(String email, String horario, String dia) {
-		return true;
+		return this.sistema.consultaHorario(email, horario, dia);
 	}
 	
 	public boolean consultaLocal(String email, String local) {
-		return true;
+		return this.sistema.consultaLocal(email, local);
 	}
 }
