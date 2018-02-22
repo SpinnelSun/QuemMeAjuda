49edf21af83211377c3ab479f66615e594fe303a
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2018-02-22 10:41:44 -0300

    Adição da Facade

---
+++ src/controllers/Facade.java
@@ -0,0 +1,48 @@
+package controllers;
+
+public class Facade {
+	
+	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
+		return;
+	}
+	
+	public String recuperaAluno(String matricula) {
+		return "";
+	}
+	
+	public String listarAlunos() {
+		return "";
+	}
+	
+	public String getInfoAluno(String matricula, String atributo) {
+		return "";
+	}
+	
+	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
+		return;
+	}
+	
+	public String recuperaTutor(String matricula) {
+		return "";
+	}
+	
+	public String listarTutores() {
+		return "";
+	}
+	
+	public void cadastrarHorario(String email, String horario, String dia) {
+		return;
+	}
+	
+	public void cadastrarLocalDeAtendimento(String email, String local) {
+		return;
+	}
+	
+	public boolean consultaHorario(String email, String horario, String dia) {
+		return true;
+	}
+	
+	public boolean consultaLocal(String email, String local) {
+		return true;
+	}
+}
