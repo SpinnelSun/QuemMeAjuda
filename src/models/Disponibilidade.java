892f4449168d4948191d485b3f31d473b921e6b5
SpinnelSun <m.hollygard@gmail.com>
2018-02-27 20:47:10 -0300

    Pequenos ajustes

--- src/models/Disponibilidade.java
+++ src/models/Disponibilidade.java
@@ -21,20 +21,20 @@ public class Disponibilidade {
 		this.horarios.add(new Horario(hora, dia));
 	}
 	
-	public int totalLocaisCadastrados() {
-		return this.locais.size();
-	}
-	
-	public int totalHorariosCadastrados() {
-		return this.horarios.size();
-	}
-	
 	public boolean verificarHorarioCadastrado(String hora, String dia) {
 		return this.horarios.contains(new Horario(hora, dia));
 	}
 	
 	public boolean verificarLocalCadastrado(String nomeLocal) {
 		return this.locais.contains(new Local(nomeLocal));
+	}
+	
+	public int totalLocaisCadastrados() {
+		return this.locais.size();
+	}
+	
+	public int totalHorariosCadastrados() {
+		return this.horarios.size();
 	}
 
 }
