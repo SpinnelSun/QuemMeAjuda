4dc7a31752f5654c2a19f6714283a9b7541acd18
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2018-02-27 22:26:47 -0300

    Documentação da classe Aluno

--- src/models/Aluno.java
+++ src/models/Aluno.java
@@ -2,12 +2,34 @@ package models;
 
 import utility.Validador;
 
+/**
+ * Representacao de um Aluno do sistema de ajuda. Todo aluno deve ter um nome, uma matricula, o co-
+ * digo do curso no qual esta matriculado, um email, uma nota (inicializada com valor 5.0) e, opcio-
+ * nalmente, um telefone.
+ * 
+ * @author Mateus de Lima Oliveira  - 117110219
+ * @author Misael Augusto Silva da Costa - 117110525
+ * @author Matheus Alves dos Santos - 117110503
+ *
+ */
 public class Aluno extends Pessoa {
 	
 	private String matricula;
 	private int codigoCurso;
 	private double nota;
 	
+	/**
+	 * Constroi um objeto do tipo Aluno a partir do nome, matricula, codigo do curso, telefone e
+	 * email. Alem disso, verifica e trata invalidez nos atributos 'matricula', caso seja uma String
+	 * vazia ou nula, e 'codigoCurso', caso seja um valor menor que 1.
+	 * 
+	 * 
+	 * @param nome O nome do aluno
+	 * @param matricula A matricula do aluno
+	 * @param codigoCurso O codigo do curso no qual o aluno esta matriculado
+	 * @param telefone O telefone do aluno
+	 * @param email O email do aluno
+	 */
 	public Aluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
 		super(nome, telefone, email);
 		
@@ -35,6 +57,17 @@ public class Aluno extends Pessoa {
 		this.nota = nota;
 	}
 
+	/**
+	 * Monta e retorna uma mensagem em String da representacao textutal do objeto Aluno a partir dos
+	 * seus atributos: matricula, nome, codigoCurso, telefone (caso nao seja vazio) e email respecti-
+	 * vamente. Se o atributo telefone for uma String vazia, significa que o aluno nao possui o mes-
+	 * mo, sendo assim, a representacao textual para este caso desconsidera o atributo telefone.
+	 * 
+	 * "matricula - nome - codigoCurso - telefone - email"
+	 * "matricula - nome - codigoCurso - email" 
+	 * 
+	 * @returns A representacao textual do Aluno.
+	 */
 	@Override
 	public String toString() {
 		String toString = this.getMatricula() + " - " + this.getNome() + " - " + this.getCodigoCurso() + " - ";
