d4d5a7fc24f7bcb9a5d70642f0932d6bf228b8f1
SpinnelSun <m.hollygard@gmail.com>
2018-02-27 22:57:57 -0300

    Adição da documentação de Facade

--- src/controllers/Facade.java
+++ src/controllers/Facade.java
@@ -2,6 +2,16 @@ package controllers;
 
 import easyaccept.EasyAccept;
 
+/**
+ * Facade Controller do Quem Me Ajuda.
+ * 
+ * Laboratorio de Programacao 2 - Projeto de Laboratorio - Quem Me Ajuda
+ * 
+ * @author Mateus de Lima Oliveira  - 117110219
+ * @author Matheus Alves dos Santos - 117110503
+ * @author Misael Augusto Silva da Costa - 117110525
+ *
+ */
 public class Facade {
 	
 	private static Sistema sistema;
@@ -14,51 +24,179 @@ public class Facade {
 		EasyAccept.main(args);
 	}
 	
+	/**
+	 * Inicializa o Sistema do Quem Me Ajuda.
+	 * 
+	 * @returns null.
+	 * 
+	 * @see Sistema#Sistema()
+	 * 
+	 */
 	public static void inicializar() {
 		Facade.sistema = new Sistema();
 	}
 	
+	/**
+	 * Cadastra um novo Aluno no Sistema.
+	 * 
+	 * @param nome O nome do Aluno a ser cadastrado.
+	 * @param matricula A matrícula do Aluno a ser cadastrado.
+	 * @param codigoCurso O código do curso do Aluno a ser cadastrado.
+	 * @param telefone O telefone do Aluno (se possuir) a ser cadastrado.
+	 * @param email O email do Aluno a ser cadastrado.
+	 * 
+	 * @returns null.
+	 * 
+	 * @see Sistema#cadastrarAluno(String, String, int, String, String)
+	 * 
+	 */
 	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
 		Facade.sistema.cadastrarAluno(nome, matricula, codigoCurso, telefone, email);
 	}
 	
+	/**
+	 * Retorna a representação textual de um Aluno já cadastrado no Sistema.
+	 * 
+	 * @param matricula A matrícula do Aluno a ser recuperado.
+	 * 
+	 * @returns A representação textual do Aluno.
+	 * 
+	 * @see Sistema#recuperaAluno(String)
+	 * 
+	 */
 	public String recuperaAluno(String matricula) {
 		return Facade.sistema.recuperaAluno(matricula);
 	}
 	
+	/**
+	 * Lista os Alunos atualmente cadastrados no Sistema do Quem Me Ajuda.
+	 * 
+	 * @returns A listagem dos Alunos cadastrados.
+	 * 
+	 * @see Sistema#listarAlunos()
+	 * 
+	 */
 	public String listarAlunos() {
 		return Facade.sistema.listarAlunos();
 	}
 	
+	/**
+	 * Recupera uma informação específica de um Aluno já cadastrado no Sistema.
+	 * 
+	 * @param matricula A matrícula do Aluno de interesse.
+	 * @param atributo A informação de interesse a ser recuperada.
+	 * 
+	 * @returns A representação textual da informação solicitada ao Sistema.
+	 * 
+	 * @see Sistema#getInfoAluno(String, String)
+	 * 
+	 */
 	public String getInfoAluno(String matricula, String atributo) {
 		return Facade.sistema.getInfoAluno(matricula, atributo);
 	}
 	
+	/**
+	 * Garante a Tutoria de uma certa disciplina a um Aluno já cadastrado no Sistema.
+	 * 
+	 * @param matricula A matrícula do Aluno que se tornará Tutor.
+	 * @param disciplina A disciplina que receberá o novo Tutor.
+	 * @param proficiencia O nível de habilidade do Aluno na disciplina.
+	 * 
+	 * @returns null.
+	 * 
+	 * @see Sistema#tornarTutor(String, String, int)
+	 * 
+	 */
 	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
 		Facade.sistema.tornarTutor(matricula, disciplina, proficiencia);
 	}
 	
+	/**
+	 * Recupera a representação textual de um Tutor já cadastrado no Sistema.
+	 * 
+	 * @param matricula A matrícula do Tutor a ser recuperado.
+	 * 
+	 * @returns A representação textual do Tutor recuperado.
+	 * 
+	 * @see Sistema#recuperaTutor(String)
+	 * 
+	 */
 	public String recuperaTutor(String matricula) {
 		return Facade.sistema.recuperaTutor(matricula);
 	}
 	
+	/**
+	 * Lista os Tutores atualmente cadastrados no Sistema do Quem Me Ajuda.
+	 * 
+	 * @returns A listagem dos Tutores cadastrados.
+	 * 
+	 * @see Sistema#listarTutores()
+	 * 
+	 */
 	public String listarTutores() {
 		return Facade.sistema.listarTutores();
 	}
 	
+	/**
+	 * Torna um Tutor disponível em um determinado Horario da semana.
+	 * 
+	 * @param email O email do Tutor que possuirá o Horario disponível.
+	 * @param horario A hora a ser cadastrada como disponível.
+	 * @param dia O dia da semana referente ao Horario disponível.
+	 * 
+	 * @returns null.
+	 * 
+	 * @see Sistema#cadastrarHorario(String, String, String)
+	 * 
+	 */
 	public void cadastrarHorario(String email, String horario, String dia) {
 		Facade.sistema.cadastrarHorario(email, horario, dia); 
 	}
 	
+	/**
+	 * Torna um Tutor apto a atender os Alunos em um determinado Local.
+	 * 
+	 * @param email O email do Tutor que poderá atender no Local.
+	 * @param local O nome do Local de atendimento a ser cadastrado.
+	 * 
+	 * @returns null.
+	 * 
+	 * @see Sistema#cadastrarLocalDeAtendimento(String, String)
+	 * 
+	 */
 	public void cadastrarLocalDeAtendimento(String email, String local) {
 		Facade.sistema.cadastrarLocalDeAtendimento(email, local);
 	}
 	
+	/**
+	 * Verifica se um Tutor está disponível em um determinado Horario.
+	 * 
+	 * @param email O email do Tutor a ser verificado.
+	 * @param horario A hora de interesse na verificação.
+	 * @param dia O dia de interesse na verificação.
+	 * 
+	 * @returns O boolean referente à verificação de disponibilidade.
+	 * 
+	 * @see Sistema#consultaHorario(String, String, String)
+	 * 
+	 */
 	public boolean consultaHorario(String email, String horario, String dia) {
 		return Facade.sistema.consultaHorario(email, horario, dia);
 	}
 	
+	/**
+	 * Verifica se um Tutor está disponível para atender em um determinado Local.
+	 * 
+	 * @param email O email do Tutor a ser verificado.
+	 * @param local O nome do local de interesse na verificação.
+	 * 
+	 * @returns O boolean referente à verificação de disponibilidade.
+	 * 
+	 * @see Sistema#consultaLocal(String, String)
+	 * 
+	 */
 	public boolean consultaLocal(String email, String local) {
 		return Facade.sistema.consultaLocal(email, local);
 	}
+	
 }
