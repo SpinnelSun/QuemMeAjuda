90387cec3ef1f537de06fbf34ffb7b386d0ea494
Mateus de Lima <mateus.oliveira@ccc.ufcg.edu.br>
2018-02-27 21:16:47 -0300

    Testes e documentação de Sistema.

--- src/controllers/Sistema.java
+++ src/controllers/Sistema.java
@@ -11,24 +11,62 @@ import models.Aluno;
 import models.Tutor;
 import utility.Validador;
 
+/**
+ * Representação de um Sistema que tem como objetivo aproximar alunos
+ * que precisam de ajuda em disciplinas dos demais alunos capacitados
+ * para oferecer ajuda. No sistema, o aluno pode realizar seu cadastro e 
+ * se colocar como tutor. O software deve dar suporte a listagem de 
+ * alunos e tutores e deve permitir a criação de ajudas.
+ * 
+ * Laboratório de Programação 2 - Projeto - Quem me ajuda
+ * 
+ * @author Mateus de Lima Oliveira  - 117110219
+ * @author Misael Augusto Silva da Costa - 117110525
+ * @author Matheus Alves dos Santos - 117110503
+ *
+ */
 public class Sistema {
 	
 	private Map<String, Aluno> alunos;
 	private Map<String, Tutor> tutores;
 	private Comparator<Aluno> ordenadorAlunos;
 	
+	
+	/**
+	 * Constrói um Sistema.
+	 * 
+	 */
 	public Sistema() {
 		this.alunos = new HashMap<String, Aluno>();
 		this.tutores = new HashMap<String, Tutor>();
 		this.ordenadorAlunos = new AlunoPorNome();
 	}
 	
+	/**
+	 * Verifica se determinada matricula já está no sistema.
+	 * 
+	 * @param matricula Matricula do aluno
+	 * 
+	 * @returns null
+	 */
 	private void verificarAlunoRepetido(String matricula, String msg) {
 		if (this.alunos.containsKey(matricula)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 	
+	/**
+	 * Cadastra um anulo no sistema. Não é permitido o cadastro de alunos repetidos, com dados vazios ou nulos ou email inválido. Porém, telefone pode ser vazio
+	 * 
+	 * @param nome O nome do Aluno.
+	 * @param matricula A matricula do Aluno. 
+	 * @param codigoCurso Codigo do curso do Aluno.
+	 * @param telefone Numero do telefone do Aluno.
+	 * @param email Email do Aluno.
+	 * 
+	 * @returns null.
+	 * 
+	 */
 	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
 		try {
 			Aluno aluno = new Aluno(nome, matricula, codigoCurso, telefone, email);
@@ -46,12 +84,27 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Verifica se a matricula passada existe ou não no sistema.
+	 * 
+	 * @param matricula Representação de matricula.
+	 * @param msg Mensagem à ser retornada ao user caso tenha algum erro.
+	 * 
+	 * @returns null
+	 */
 	private void verificarAlunoInexistente(String matricula, String msg) {
 		if (!this.alunos.containsKey(matricula)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 	
+	/**
+	 * Recupera representação de Aluno contendo suas informações caso os dados passados estejam corretos.
+	 * 
+	 * @param matricula Representação de matricula do aluno.
+	 * 
+	 * @returns String Representação de Aluno.
+	 */
 	public String recuperaAluno(String matricula) {
 		try {
 			this.verificarAlunoInexistente(matricula, "Aluno nao encontrado");
@@ -63,6 +116,12 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Ordena os alunos por ordem alfabetica dos seus respectivos nomes.
+	 * 
+	 * 
+	 * @returns List<Alunos> Lista de alunos em ordem.
+	 */
 	private List<Aluno> ordenarAlunos() {
 		List<Aluno> alunosPorNome = new ArrayList<Aluno>();
 		alunosPorNome.addAll(this.alunos.values());
@@ -71,6 +130,12 @@ public class Sistema {
 		return alunosPorNome;
 	}
 	
+	/**
+	 * Recupera lista de alunos registrados no sistema por ordem alfabetica.
+	 * 
+	 * 
+	 * @returns String Representação de todos os alunos separados por vírgula.
+	 */
 	public String listarAlunos() {
 		String listagemAlunos = "";
 		for (int i = 0; i < this.ordenarAlunos().size() - 1; i++) {
@@ -81,6 +146,14 @@ public class Sistema {
 		return listagemAlunos;
 	}
 	
+	/**
+	 * Recupera determinado atributo de Aluno. Tal atributo é passado nos parametros e não são aceitos atributos vazios ou nulos
+	 * 
+	 * @param matricula Representação de matricula do aluno.
+	 * @param atribulo Atributo requisitado. 
+	 * 
+	 * @returns String Valor do atributo requisitado.
+	 */
 	public String getInfoAluno(String matricula, String atributo) {
 		try {
 			this.verificarAlunoInexistente(matricula, "Aluno nao encontrado");
@@ -92,12 +165,28 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Registra no sistema um novo tutor com base nos seus atributos de Aluno.
+	 * 
+	 * @param matricula Representação de matricula do aluno.
+	 * 
+	 * @returns null
+	 */
 	private void criarNovoTutor(String matricula) {
 		Aluno aluno = alunos.get(matricula);
 		this.tutores.put(aluno.getMatricula(), new Tutor(aluno.getNome(), aluno.getMatricula(),
 			    aluno.getCodigoCurso(), aluno.getTelefone(), aluno.getEmail()));
 	}
 	
+	/**
+	 * Torna Aluno um Tutor, não sendo possivel matriculas invalidas, proficiencias erradas e disciplinas vazias ou nulas. 
+	 * 
+	 * @param matricula Representação de matricula do aluno.
+	 * @param disciplina Disciplina que o aluno será tutor.
+	 * @param proficiencia Avaliação de sua tutoria.
+	 * 
+	 * @returns null
+	 */
 	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
 		try {
 			this.verificarAlunoInexistente(matricula, "Tutor nao encontrado");
@@ -114,6 +203,13 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Recupera representação de Tutor contendo suas informações caso os dados passados estejam corretos.
+	 * 
+	 * @param matricula Representação de matricula do tutor.
+	 * 
+	 * @returns String Representação de Tutor.
+	 */
 	public String recuperaTutor(String matricula) {
 		try {
 			this.verificarAlunoInexistente(matricula, "Tutor nao encontrado");
@@ -126,6 +222,12 @@ public class Sistema {
 		
 	}
 	
+	/**
+	 * Gera lista de tutores para facilitar verificações.
+	 * 
+	 * 
+	 * @returns List<Tutor> Lista de tutores cadastrados.
+	 */
 	private List<Tutor> tutoresToList() {
 		List<Tutor> listaDeTutores = new ArrayList<Tutor>();
 		listaDeTutores.addAll(this.tutores.values());
@@ -133,6 +235,12 @@ public class Sistema {
 		return listaDeTutores;
 	}
 	
+	/**
+	 * Recupera lista de tutores registrados no sistema.
+	 * 
+	 * 
+	 * @returns String Representação de todos os tutores separados por vírgula.
+	 */
 	public String listarTutores() {
 		String listagemTutores = "";
 		
@@ -144,6 +252,13 @@ public class Sistema {
 		return listagemTutores;
 	}
 	
+	/**
+	 * Recupera matricula de tutores a partir do email do mesmo.
+	 * 
+	 * @param email Email do tutor.
+	 * 
+	 * @returns String matricula do tutor.
+	 */
 	private String getMatriculaPorEmail(String email) {
 		for(Tutor tutor : this.tutoresToList()) {
 			if(tutor.getEmail().equals(email)) {
@@ -154,6 +269,15 @@ public class Sistema {
 		return "";
 	}
 	
+	/**
+	 * Cadastra horário de atendimento do tutor aos demais alunos. Não são permitidos dados vazios ou nulos. 
+	 * 
+	 * @param email Email do tutor.
+	 * @param horario Horario disponivel para atendimento.
+	 * @param dia Dia que serão disponibilizadas as tutorias. 
+	 * 
+	 * @returns null
+	 */
 	public void cadastrarHorario(String email, String horario, String dia) {
 		try {
 			Validador.validarStringNaoVaziaNaoNula("email nao pode ser vazio ou em branco", email);
@@ -167,6 +291,14 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Cadastra local de atendimento do tutor aos demais alunos. Não são permitidos dados vazios ou nulos. 
+	 * 
+	 * @param email Email do tutor.
+	 * @param local Local disponivel para atendimento dos alunos.
+	 * 
+	 * @returns null
+	 */
 	public void cadastrarLocalDeAtendimento(String email, String local) {
 		try {
 			Validador.validarStringNaoVaziaNaoNula("email nao pode ser vazio ou em branco", email);
@@ -180,6 +312,16 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Verifica horário de atendimento do tutor aos demais alunos, caso o horario e dia estejam disponiveis é retornado True, se não, é 
+	 * retornado False. Não são permitidos dados vazios ou nulos. 
+	 * 
+	 * @param email Email do tutor.
+	 * @param horario Horario disponivel para atendimento.
+	 * @param dia Dia que serão disponibilizadas as tutorias. 
+	 * 
+	 * @returns boolean True se houver o horario ou False se não houver.
+	 */
 	public boolean consultaHorario(String email, String horario, String dia) {
 		try {
 			Validador.validarStringNaoVaziaNaoNula("email nao pode ser vazio ou em branco", email);
@@ -193,6 +335,15 @@ public class Sistema {
 		}
 	}
 	
+	/**
+	 * Verifica o local de atendimento do tutor aos demais alunos, caso o local esteja correto é retornado True, se não, é 
+	 * retornado False. Não são permitidos dados vazios ou nulos. 
+	 * 
+	 * @param email Email do tutor.
+	 * @param local Local disponivel para atendimento dos alunos.
+	 * 
+	 * @returns boolean True se houver o local registrado ou False se não houver.
+	 */
 	public boolean consultaLocal(String email, String local) {
 		try {
 			Validador.validarStringNaoVaziaNaoNula("email nao pode ser vazio ou em branco", email);
--- unt-tst/controllers/SistemaTest.java
+++ unt-tst/controllers/SistemaTest.java
@@ -12,13 +12,34 @@ public class SistemaTest {
 	@Before
 	public void inicializar() {
 		s = new Sistema();
-		s.cadastrarAluno("Mateus de Lima Oliveira", "118110219", 001, "(83) 99337-5161", "mateuslm@live.com");
+		
+		s.cadastrarAluno("Mateus de Lima Oliveira", "118110219", 1, "(83) 99337-5161", "mateuslm@live.com");
+		s.cadastrarAluno("Matthew Melio", "115260904", 10000,"", "matthew.met@ccc.ufcg.edu.br");
+		
+		s.tornarTutor("115260904", "ATAL", 3);
+		s.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", "Seg");	
+		s.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", "CAA - 104");
 	}
 	
 	@Test
 	public void testCadastrarAluno() {
 		s.cadastrarAluno("Mateus de Lima Oliveira", "117110219", 001, "(83) 99337-5161", "mateuslm@live.com");
 	} 
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testCadastrarAlunoNomeVazio() {
+		s.cadastrarAluno("", "117110219", 001, "(83) 99337-5161", "mateuslm@live.com");
+	} 
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testCadastrarAlunoEmailVazio() {
+		s.cadastrarAluno("Mateus de Lima Oliveira", "117110219", 001, "(83) 99337-5161", "");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testCadastrarAlunoMatriculaVazio() {
+		s.cadastrarAluno("Mateus de Lima Oliveira", "", 001, "(83) 99337-5161", "mateuslm@live.com");
+	}
 	
 	@Test
 	public void testCadastrarAlunoTelefoneVazio() {
@@ -40,9 +61,154 @@ public class SistemaTest {
 		assertEquals(s.getInfoAluno("118110219", "Telefone"), "(83) 99337-5161");
 	} 
 	
+	@Test (expected = IllegalArgumentException.class)
+	public void testGetInfoAlunoAtributoInvalido() {
+		assertEquals(s.getInfoAluno("118110219", "Idade"), "(83) 99337-5161");
+	} 
+	
+	@Test
+	public void testRecuperarAluno() {
+		assertEquals(s.recuperaAluno("118110219"), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com");
+	} 
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testRecuperarAlunoMatriculaInvalida() {
+		assertEquals(s.recuperaAluno("018110219"), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com");
+	} 
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testRecuperarAlunoMatriculaVazia() {
+		assertEquals(s.recuperaAluno(""), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com");
+	} 
+	
+	@Test
+	public void testListarAlunos() {
+		assertEquals(s.listarAlunos(), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com, 115260904 - Matthew Melio - 10000 - matthew.met@ccc.ufcg.edu.br");
+	}
+	
+	@Test
+	public void testTornarTutor() {
+		s.tornarTutor("118110219", "Vetorial", 1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTornarTutorMatriculaInvalida() {
+		s.tornarTutor("117110219", "Vetorial", 1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTornarTutorProeficienciaInvalida() {
+		s.tornarTutor("117110219", "Vetorial", -1);
+	}
+	
+	@Test
+	public void testRecuperarTutor() {
+		assertEquals(s.recuperaTutor("115260904"), "115260904 - Matthew Melio - 10000 - matthew.met@ccc.ufcg.edu.br");
+	} 
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testRecuperarTutorMatriculaInvalida() {
+		assertEquals(s.recuperaTutor("015260904"), "115260904 - Matthew Melio - 10000 - matthew.met@ccc.ufcg.edu.br");
+	}
+	
+	@Test
+	public void testListarTutores() {
+		assertEquals(s.listarTutores(), "115260904 - Matthew Melio - 10000 - matthew.met@ccc.ufcg.edu.br");
+	}
+	
 	@Test
 	public void testCadastrarHorario() {
-		
+		s.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", "Seg");		
 	} 
-
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrarHorarioEmailVazio() {
+		s.cadastrarHorario("", "15:30", "Seg");		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrarHorarioHorarioVazio() {
+		s.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "", "Seg");		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrarHorarioDiaVazio() {
+		s.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", "");		
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastrarHorarioEmailNulo() {
+		s.cadastrarHorario(null, "15:30", "Seg");		
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastrarHorarioHorarioNulo() {
+		s.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", null, "Seg");		
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastrarHorarioDiaNulo() {
+		s.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", null);		
+	}
+	
+	@Test
+	public void testCadastrarLocalDeAtendimento() {
+		s.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", "CAA - 104");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrarLocalDeAtendimentoEmailVazio() {
+		s.cadastrarLocalDeAtendimento("", "CAA - 104");		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrarLocalDeAtendimentoLocalVazio() {
+		s.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", "");		
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastrarLocalDeAtendimentoEmailNulo() {
+		s.cadastrarLocalDeAtendimento(null, "CAA - 104");		
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastrarLocalDeAtendimentoLocalNulo() {
+		s.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", null);		
+	}
+	
+	@Test
+	public void testConsultarLocalDeAtendimento() {
+		s.consultaLocal("matthew.met@ccc.ufcg.edu.br", "CAA - 104");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testConsultarLocalDeAtendimentoEmailNulo() {
+		s.consultaLocal(null, "CAA - 104");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testConsultarLocalDeAtendimentoLocalNulo() {
+		s.consultaLocal("matthew.met@ccc.ufcg.edu.br", null);
+	}
+	
+	@Test
+	public void testConsultarHorarioDeAtendimento() {
+		s.consultaHorario("matthew.met@ccc.ufcg.edu.br", "15:30", "Seg");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testConsultarHorarioDeAtendimentoEmailNulo() {
+		s.consultaHorario(null, "15:30", "Seg");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testConsultarHorarioDeAtendimentoHorarioNulo() {
+		s.consultaHorario("matthew.met@ccc.ufcg.edu.br", null, "Seg");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testConsultarHorarioDeAtendimentoDiaNulo() {
+		s.consultaHorario("matthew.met@ccc.ufcg.edu.br", "15:30", null);
+	}
+	
 }
