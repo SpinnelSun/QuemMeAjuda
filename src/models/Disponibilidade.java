011cf18e4f37ddc21f8e28e89c64f61758e3e262
Mateus de Lima <mateus.oliveira@ccc.ufcg.edu.br>
2018-03-06 21:01:21 -0300

    Documentação de  Disponibilidade

--- src/models/Disponibilidade.java
+++ src/models/Disponibilidade.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 /**
  * Representacao de uma Disponibilidade a ser registrada num Tutor. Como atributos, uma Disponibili-
- * dade possui um conjunto de locais e um conjunto de horarios. A Disponibilidade refere-se a um con-
- * junto de locais e horarios nos quais o tutor estara disponivel para atendimento.
+ * dade possui um conjunto de locais e um conjunto de horarios. A Disponibilidade refere-se aos lo-
+ * cais e horarios nos quais o tutor estara disponivel para atendimento.
  * 
  * Laboratorio de Programacao 2 - Projeto de Laboratorio - Quem Me Ajuda
  * 
@@ -21,7 +21,8 @@ public class Disponibilidade {
 	private Set<Horario> horarios;
 	
 	/**
-	 * Constroi um objeto Disponibilidade iniciando seus atributos como conjuntos vazios.
+	 * Constroi uma Disponibilidade cujos parametros locais e horarios sao iniciados como Sets va-
+	 * zios de Local e Horario, respectivamente.
 	 * 
 	 */
 	public Disponibilidade() {
@@ -30,10 +31,10 @@ public class Disponibilidade {
 	}
 	
 	/**
-	 * Cria e adiciona um Local ao conjunto de locais da Disponibilidade a partir do nome do Local
-	 * passado como parametro.
+	 * Cria um novo Local cujo nome sera a String passada como parametro desse metodo. O Local cria-
+	 * do sera adicionado ao atributo locais da Disponibilidade.
 	 * 
-	 * @param nomeLocal O nome do Local.
+	 * @param nomeLocal O nome do Local disponivel disponivel para atendimento.
 	 * 
 	 * @returns null.
 	 * 
@@ -43,11 +44,11 @@ public class Disponibilidade {
 	}
 	
 	/**
-	 * Cria e adiciona um Horario ao conjunto de Horarios da Disponibilidade a partir da hora e do
-	 * dia do Horario passados como parametro.
+	 * Cria um novo Horario cuja hora e dia serao as Strings passadas como parametro desse metodo.
+	 * O Horario criado sera adicionado ao atributo horarios da Disponibilidade.
 	 * 
-	 * @param hora A hora em que ha disponibilidade.
-	 * @param dia O dia da semana referente ao Horario.
+	 * @param hora A hora disponivel para atendimento.
+	 * @param dia O dia disponivel para atendimento.
 	 * 
 	 * @returns null.
 	 * 
@@ -57,26 +58,13 @@ public class Disponibilidade {
 	}
 	
 	/**
-	 * Verifica se um Horario esta cadastrado na Disponibilidade a partir da hora e do dia passados
-	 * como parametros. Caso esteja cadastrado, retorna true, caso contrario retorna false.
+	 * Verifica se a Disponibilidade executando esse metodo possui um Local especifico em seu Set
+	 * de Locais. A hora e o dia de interesse devem ser passados como parametros do metodo. Sera
+	 * retornado o boolean referente a disponibilidade do Local de interesse.
 	 * 
-	 * @param hora A hora em que se espera disponibilidade.
-	 * @param dia O dia da semana referente ao Horario.
+	 * @param nomeLocal O nome do local de interesse na verificacao.
 	 * 
-	 * @returns O boolean que indica a existencia do cadastro do Horario.
-	 * 
-	 */
-	public boolean verificarHorarioCadastrado(String hora, String dia) {
-		return this.horarios.contains(new Horario(hora, dia));
-	}
-
-	/**
-	 * Verifica se um local esta cadastrado no conjunto de locais a partir do nome passado como para-
-	 * metro. Caso esteja cadastrado, retorna true, caso contrario, retorna false.
-	 * 
-	 * @param nome O nome do local em que se espera disponibilidade.
-	 * 
-	 * @returns O boolean que indica a existencia do cadastro do Local.
+	 * @returns O boolean que indica a existencia de disponibilidade para o Local de interesse.
 	 * 
 	 */
 	public boolean verificarLocalCadastrado(String nomeLocal) {
@@ -84,9 +72,25 @@ public class Disponibilidade {
 	}
 	
 	/**
-	 * Retorna a quantidade de Locais cadastrados na Disponibilidade.
+	 * Verifica se a Disponibilidade executando esse metodo possui um Horario especifico em seu Set
+	 * de Horarios. A hora e o dia de interesse devem ser passados como parametros do metodo. Sera
+	 * retornado o boolean referente a disponibilidade do Horario de interesse.
 	 * 
-	 * @returns A quantidade de Locais cadastrados.
+	 * @param hora A hora de interesse na verificacao.
+	 * @param dia A dia de interesse na verificacao.
+	 * 
+	 * @returns O boolean que indica a existencia de disponibilidade para o Horario de interesse.
+	 * 
+	 */
+	public boolean verificarHorarioCadastrado(String hora, String dia) {
+		return this.horarios.contains(new Horario(hora, dia));
+	}
+
+	/**
+	 * Retorna a quantidade de Locais existentes no Set de Locais da Disponibilidade executando esse
+	 * metodo.
+	 * 
+	 * @returns A quantidade de Locais ja cadastrados.
 	 * 
 	 */
 	public int totalLocaisCadastrados() {
@@ -94,21 +98,14 @@ public class Disponibilidade {
 	}
 	
 	/**
-	 * Retorna a quantidade de Horarios cadastrados na Disponibilidade.
+	 * Retorna a quantidade de Horarios existentes no Set de Horarios da Disponibilidade executando
+	 * esse metodo.
 	 * 
-	 * @returns A quantidade de Horarios cadastrados.
+	 * @returns A quantidade de Horarios ja cadastrados.
 	 * 
 	 */
 	public int totalHorariosCadastrados() {
 		return this.horarios.size();
-	}
-
-	public Set<Local> getLocais() {
-		return locais;
-	}
-
-	public Set<Horario> getHorarios() {
-		return horarios;
 	}
 	
 }
