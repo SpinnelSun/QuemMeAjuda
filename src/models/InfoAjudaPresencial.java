f1f48f582fb25d4eb92acde6705c427d3e43cb3a
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2018-03-06 21:42:37 -0300

    Documentação das classes InfoAjudaPresencial e NivelAvaliacao

--- src/models/InfoAjudaPresencial.java
+++ src/models/InfoAjudaPresencial.java
@@ -2,6 +2,17 @@ package models;
 
 import views.SeletorInfoAjudaPresencial;
 
+/**
+ * Enumeracao das informacoes possuidas por uma AjudaPresencial. Cada valor de InfoAjudaPresencial e
+ * definido a partir de uma String que representa seu nome.
+ * 
+ * Laboratorio de Programacao 2 - Projeto de Laboratorio - Quem Me Ajuda
+ * 
+ * @author Mateus de Lima Oliveira  - 117110219
+ * @author Misael Augusto Silva da Costa - 117110525
+ * @author Matheus Alves dos Santos - 117110503
+ *
+ */
 public enum InfoAjudaPresencial implements SeletorInfoAjudaPresencial {
 	
 	DISCIPLINA("DISCIPLINA") {
@@ -48,10 +59,23 @@ public enum InfoAjudaPresencial implements SeletorInfoAjudaPresencial {
 	
 	private String nomeInfo;
 	
+	/**
+	 * Constroi um valor de InfoAjudaPresencial a partir de uma String que o defina.
+	 * 
+	 * @param estado A String que nomeia o valor de InfoAjudaPresencial.
+	 * 
+	 */
 	InfoAjudaPresencial(String nomeInfo) {
 		this.nomeInfo = nomeInfo;
 	}
 	
+	/**
+	 * Verifica se a String passada como parametro desse metodo especifica algum dos valores de
+	 * InfoAjudaPresencial. Caso nao o faca, esse metodo lancara a excecao adequada.
+	 * 
+	 * @returns null.
+	 * 
+	 */
 	public static void impedirAtributoInexistente(String atributo) {
 		for (InfoAjudaPresencial infoAjuda : InfoAjudaPresencial.values()) {
 			if (infoAjuda.toString().equals(atributo)) {
@@ -62,6 +86,12 @@ public enum InfoAjudaPresencial implements SeletorInfoAjudaPresencial {
 		throw new IllegalArgumentException("atributo nao encontrado");
 	}
 	
+	/**
+	 * Retorna a String que define o valor de InfoAjudaPresencial.
+	 * 
+	 * @returns A representacao textual de um valor de InfoAjudaPresencial.
+	 * 
+	 */
 	@Override
 	public String toString() {
 		return this.nomeInfo;
--- src/models/NivelAvaliacao.java
+++ src/models/NivelAvaliacao.java
@@ -1,5 +1,16 @@
 package models;
 
+/**
+ * Enumeracao dos niveis pelos quais um Tutor pode ser classificado a partir de sua nota de avalia-
+ * cao. Cada valor de NivelAvaliacao e definido a partir de uma String que o especifique.
+ * 
+ * Laboratorio de Programacao 2 - Projeto de Laboratorio - Quem Me Ajuda
+ * 
+ * @author Mateus de Lima Oliveira  - 117110219
+ * @author Misael Augusto Silva da Costa - 117110525
+ * @author Matheus Alves dos Santos - 117110503
+ *
+ */
 public enum NivelAvaliacao {
 	
 	ALTO("TOP") ,
@@ -10,10 +21,22 @@ public enum NivelAvaliacao {
 	
 	private String nivel;
 	
+	/**
+	 * Constroi um valor de NivelAvaliacao a partir de uma String que o especifique.
+	 * 
+	 * @param estado A String que especifica o valor de NivelAvaliacao.
+	 * 
+	 */
 	NivelAvaliacao(String nivel) {
 		this.nivel = nivel;
 	}
 	
+	/**
+	 * Retorna a String que define o valor de NivelAvaliacao.
+	 * 
+	 * @returns A representacao textual de um valor de NivelAvaliacao.
+	 * 
+	 */
 	@Override
 	public String toString() {
 		return this.nivel;
