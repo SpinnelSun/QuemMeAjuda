521da613c4cd506aa194c0b9b6a9dfd1867afb77
SpinnelSun <m.hollygard@gmail.com>
2018-03-13 15:33:57 -0300

    Padronização dos testes de unidade

--- unt-tst/controllers/SistemaTest.java
+++ unt-tst/controllers/SistemaTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import controllers.Sistema;
+import general.AlunoException;
+import general.TutorException;
 
 public class SistemaTest {
 	Sistema sistema;
@@ -26,17 +28,17 @@ public class SistemaTest {
 		this.sistema.cadastrarAluno("Mateus de Lima Oliveira", "117110219", 001, "(83) 99337-5161", "mateuslm@live.com");
 	} 
 	
-	@Test (expected = IllegalArgumentException.class)
+	@Test (expected = AlunoException.class)
 	public void testCadastrarAlunoNomeVazio() {
 		this.sistema.cadastrarAluno("", "117110219", 001, "(83) 99337-5161", "mateuslm@live.com");
 	} 
 	
-	@Test (expected = IllegalArgumentException.class)
+	@Test (expected = AlunoException.class)
 	public void testCadastrarAlunoEmailVazio() {
 		this.sistema.cadastrarAluno("Mateus de Lima Oliveira", "117110219", 001, "(83) 99337-5161", "");
 	}
 	
-	@Test (expected = IllegalArgumentException.class)
+	@Test (expected = AlunoException.class)
 	public void testCadastrarAlunoMatriculaVazio() {
 		this.sistema.cadastrarAluno("Mateus de Lima Oliveira", "", 001, "(83) 99337-5161", "mateuslm@live.com");
 	}
@@ -61,7 +63,7 @@ public class SistemaTest {
 		assertEquals(this.sistema.getInfoAluno("118110219", "Telefone"), "(83) 99337-5161");
 	} 
 	
-	@Test (expected = IllegalArgumentException.class)
+	@Test (expected = AlunoException.class)
 	public void testGetInfoAlunoAtributoInvalido() {
 		assertEquals(this.sistema.getInfoAluno("118110219", "Idade"), "(83) 99337-5161");
 	} 
@@ -71,12 +73,12 @@ public class SistemaTest {
 		assertEquals(this.sistema.recuperaAluno("118110219"), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com");
 	} 
 	
-	@Test (expected = IllegalArgumentException.class)
+	@Test (expected = AlunoException.class)
 	public void testRecuperarAlunoMatriculaInvalida() {
 		assertEquals(this.sistema.recuperaAluno("018110219"), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com");
 	} 
 	
-	@Test (expected = IllegalArgumentException.class)
+	@Test (expected = AlunoException.class)
 	public void testRecuperarAlunoMatriculaVazia() {
 		assertEquals(this.sistema.recuperaAluno(""), "118110219 - Mateus de Lima Oliveira - 1 - (83) 99337-5161 - mateuslm@live.com");
 	} 
@@ -91,12 +93,12 @@ public class SistemaTest {
 		this.sistema.tornarTutor("118110219", "Vetorial", 1);
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testTornarTutorMatriculaInvalida() {
 		this.sistema.tornarTutor("117110219", "Vetorial", 1);
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testTornarTutorProeficienciaInvalida() {
 		this.sistema.tornarTutor("117110219", "Vetorial", -1);
 	}
@@ -106,7 +108,7 @@ public class SistemaTest {
 		assertEquals(this.sistema.recuperaTutor("115260904"), "115260904 - Matthew Melio - 10000 - matthew.met@ccc.ufcg.edu.br");
 	} 
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testRecuperarTutorMatriculaInvalida() {
 		assertEquals(this.sistema.recuperaTutor("015260904"), "115260904 - Matthew Melio - 10000 - matthew.met@ccc.ufcg.edu.br");
 	}
@@ -121,32 +123,32 @@ public class SistemaTest {
 		this.sistema.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", "Seg");		
 	} 
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarHorarioEmailVazio() {
 		this.sistema.cadastrarHorario("", "15:30", "Seg");		
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarHorarioHorarioVazio() {
 		this.sistema.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "", "Seg");		
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarHorarioDiaVazio() {
 		this.sistema.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", "");		
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarHorarioEmailNulo() {
 		this.sistema.cadastrarHorario(null, "15:30", "Seg");		
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarHorarioHorarioNulo() {
 		this.sistema.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", null, "Seg");		
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarHorarioDiaNulo() {
 		this.sistema.cadastrarHorario("matthew.met@ccc.ufcg.edu.br", "15:30", null);		
 	}
@@ -156,22 +158,22 @@ public class SistemaTest {
 		this.sistema.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", "CAA - 104");
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarLocalDeAtendimentoEmailVazio() {
 		this.sistema.cadastrarLocalDeAtendimento("", "CAA - 104");		
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarLocalDeAtendimentoLocalVazio() {
 		this.sistema.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", "");		
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarLocalDeAtendimentoEmailNulo() {
 		this.sistema.cadastrarLocalDeAtendimento(null, "CAA - 104");		
 	}
 	
-	@Test(expected = NullPointerException.class)
+	@Test(expected = TutorException.class)
 	public void testCadastrarLocalDeAtendimentoLocalNulo() {
 		this.sistema.cadastrarLocalDeAtendimento("matthew.met@ccc.ufcg.edu.br", null);		
 	}
