c4e10edbe68f4d153153ee1d7f2f53008f894d5f
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2018-03-13 21:11:37 -0300

    Adição dos testes de Sistema para US4-8

--- unt-tst/controllers/SistemaTest.java
+++ unt-tst/controllers/SistemaTest.java
@@ -329,4 +329,304 @@ public class SistemaTest {
 		this.sistema.consultaHorario("adress2@email.com", "15:30", null);
 	}
 	
+	@Test
+	public void testPegarTutorAjudaOnline() {
+		String msg = "Avaliacao da obtencao das informacoes do tutor de uma ajuda online.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		
+		assertEquals(msg, "Tutor - 2, disciplina - ATAL", this.sistema.pegarTutor(1));
+	}
+	
+	@Test
+	public void testPegarTutorAjudaPresencial() {
+		String msg = "Avaliacao da obtencao das informacoes do tutor de uma ajuda presencial.";
+		
+		this.sistema.pedirAjudaPresencial("1", "ATAL", "10:00", "ter", "CAA");
+		
+		assertEquals(msg, "Tutor - 2, horario - 10:00, dia - ter, local - CAA, disciplina - ATAL", this.sistema.pegarTutor(1));
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testPegarTutorIdInexistente() {
+		String msg = "Avaliacao da excecao lancada ao tentar pegar as informacoes de uma ajuda com id nao cadastrado.";
+		
+		this.sistema.pegarTutor(6);
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testPegarTutorIdNulo() {
+		String msg = "Avaliacao da excecao lancada ao tentar pegar as informacoes de uma ajuda a partir de um id nulo.";
+		
+		this.sistema.pegarTutor(0);
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testPegarTutorIdNegativo() {
+		String msg = "Avaliacao da excecao lancada ao tentar pegar as informacoes de uma ajuda a partir de um id negativo.";
+		
+		this.sistema.pegarTutor(-3);
+	}
+	
+	@Test
+	public void testGetInfoAjudaOnline() {
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		
+		String msg = "Avaliacao da obtencao da matricula do aluno que pediu a ajuda.";
+		assertEquals(msg, "1", this.sistema.getInfo(1, "aluno"));
+		
+		String msg2 = "Avaliacao da obtencao da disciplina na qual o aluno pediu a ajuda.";
+		assertEquals(msg2, "ATAL", this.sistema.getInfo(1, "disciplina"));
+		
+		String msg3 = "Avaliacao da obtencao da matricula do tutor que ira ajudar o aluno.";
+		assertEquals(msg3, "2", this.sistema.getInfo(1, "tutor"));
+	}
+	
+	@Test
+	public void testGetInfoAjudaPresencial() {
+		this.sistema.pedirAjudaPresencial("1", "ATAL", "10:00", "ter", "CAA");
+		
+		String msg = "Avaliacao da obtencao da matricula do aluno que pediu a ajuda.";
+		assertEquals(msg, "1", this.sistema.getInfo(1, "aluno"));
+		
+		String msg2 = "Avaliacao da obtencao da disciplina na qual o aluno pediu a ajuda.";
+		assertEquals(msg2, "ATAL", this.sistema.getInfo(1, "disciplina"));
+		
+		String msg3 = "Avaliacao da obtencao do horario de atendimento no qual o tutor ira ajudar o aluno.";
+		assertEquals(msg3, "10:00", this.sistema.getInfo(1, "horario"));
+		
+		String msg4 = "Avaliacao da obtencao do dia de atendimento no qual o tutor ira ajudar o aluno.";
+		assertEquals(msg4, "ter", this.sistema.getInfo(1, "dia"));
+		
+		String msg5 = "Avaliacao da obtencao do local de atendimento no qual o tutor ira ajudar o aluno.";
+		assertEquals(msg5, "CAA", this.sistema.getInfo(1, "localInteresse"));
+		
+		String msg6 = "Avaliacao da obtencao da matricula do tutor que ira ajudar o aluno.";
+		assertEquals(msg6, "2", this.sistema.getInfo(1, "tutor"));
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testGetInfoAjudaIdInexistente() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar um atributo de uma ajuda a partir de um id nao cadastrado.";
+		
+		this.sistema.getInfo(7, "aluno");
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testGetInfoAjudaIdNulo() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar um atributo de uma ajuda a partir de um id nulo.";
+		
+		this.sistema.getInfo(0, "aluno");
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testGetInfoAjudaIdNegativo() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar um atributo de uma ajuda a partir de um id negativo.";
+		
+		this.sistema.getInfo(-5, "aluno");
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testGetInfoAjudaAtributoInexistente() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar um atributo inexistente.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		this.sistema.getInfo(1, "saude");
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testGetInfoAjudaAtributoVazio() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar um atributo de uma ajuda a partir de uma String vazia.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		this.sistema.getInfo(1, "   ");
+	}
+	
+	@Test(expected=AjudaException.class)
+	public void testGetInfoAjudaAtributoNulo() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar um atributo de uma ajuda a partir de um null.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		this.sistema.getInfo(1, null);
+	}
+	
+	@Test
+	public void testAvaliarTutor() {
+		String msg = "Avaliacao do valor adequado da nota do tutor apos ser avaliado.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		this.sistema.avaliarTutor(1, 4);
+		
+		assertEquals(msg, "4,00", this.sistema.pegarNota("2"));
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testAvaliarTutorIdInexistente() {
+		String msg = "Avaliacao da excecao lancada ao tentar avaliar um tutor a partir de um id nao cadastrado.";
+		
+		this.sistema.avaliarTutor(5, 4);
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testAvaliarTutorIdNulo() {
+		String msg = "Avaliacao da excecao lancada ao tentar avaliar um tutor a partir de um id nulo.";
+		
+		this.sistema.avaliarTutor(0, 4);
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testAvaliarTutorIdNegativo() {
+		String msg = "Avaliacao da excecao lancada ao tentar avaliar um tutor a partir de um id negativo.";
+		
+		this.sistema.avaliarTutor(-6, 4);
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testAvaliarTutorNotaInvalida() {
+		String msg = "Avaliacao da excecao lancada ao tentar avaliar um tutor com nota superior a 5.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		this.sistema.avaliarTutor(1, 6);
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testAvaliarTutorNotaNegativa() {
+		String msg = "Avaliacao da excecao lancada ao tentar avaliar um tutor com nota inferior a 0.";
+		
+		this.sistema.pedirAjudaOnline("1", "ATAL");
+		this.sistema.avaliarTutor(1, -1);
+	}
+	
+	@Test
+	public void testPegarNivelTutor() {
+		String msg = "Avaliacao da obtencao do nivel adequado do tutor.";
+		
+		assertEquals(msg, this.sistema.pegarNivel("2"), "Tutor");
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testPegarNivelTutorNaoCadastrado() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar o nivel de um tutor a partir de uma matricula nao cadastrada.";
+		
+		this.sistema.pegarNivel("5");
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testPegarNivelTutorMatriculaVaiza() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar o nivel de um tutor a partir de uma String vazia.";
+		
+		this.sistema.pegarNivel("");
+	}
+	
+	@Test(expected=AvaliacaoException.class)
+	public void testPegarNivelTutorMatriculaNula() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar o nivel de um tutor a partir de um null.";
+		
+		this.sistema.pegarNivel(null);
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testDoarMatriculaNaoCadastrada() {
+		String msg = "Avaliacao da excecao lancada ao tentar doar para um tutor a partir de uma matricula nao cadastrada.";
+		
+		this.sistema.doar("5", 1000);
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testDoarMatriculaVazia() {
+		String msg = "Avaliacao da excecao lancada ao tentar doar para um tutor a partir de uma String vazia.";
+		
+		this.sistema.doar("   ", 1000);
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testDoarMatriculaNula() {
+		String msg = "Avaliacao da excecao lancada ao tentar doar para um tutor a partir de um null.";
+		
+		this.sistema.doar(null, 1000);
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testDoarValorNegativo() {
+		String msg = "Avaliacao da excecao lancada ao tentar doar para um tutor um valor negativo.";
+		
+		this.sistema.doar("2", -3);
+	}
+	
+	@Test
+	public void testGetTotalDinheiroTutor() {
+		String msg = "Avaliacao da obtencao do valor total recebido pelo tutor.";
+		
+		assertTrue(msg, this.sistema.totalDinheiroTutor("adress2@email.com") == 0);
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testGetTotalDinheiroTutorMatriculaNaoCadastrada() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar o total de dinheiro doado a um tutor a partir de um email nao cadastrada.";
+		
+		this.sistema.totalDinheiroTutor("adress5@email.com");
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testGetTotalDinheiroTutorMatriculaVazia() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar o total de dinheiro doado a um tutor a partir de uma String vazia.";
+		
+		this.sistema.totalDinheiroTutor("  ");
+	}
+	
+	@Test(expected=TutorException.class)
+	public void testGetTotalDinheiroTutorMatriculaNula() {
+		String msg = "Avaliacao da excecao lancada ao tentar resgatar o total de dinheiro doado a um tutor a partir de um null.";
+		
+		this.sistema.totalDinheiroTutor(null);
+	}
+	
+	@Test
+	public void testGetTotalSistema() {
+		String msg = "Avaliacao do armazenamento adequado do valor destinado ao caixa do sistema.";
+		
+		assertTrue(msg, this.sistema.getCaixa() == 0);
+	}
+	
+	@Test
+	public void testConfigurarOrdemAlunos() {
+		String msg = "Avaliacao da listagem adequada dos alunos apos ordenacao.";
+		
+		this.sistema.configurarOrdem("nome");
+		
+		String listagemEsperada = "1 - Nome 1 - 1 - 00000-0000 - adress@email.com, 2 - Nome 2 - 2 - adress2@email.com";
+		assertEquals(msg, listagemEsperada, this.sistema.listarAlunos());
+	}
+	
+	@Test
+	public void testConfigurarOrdemTutores() {
+		String msg = "Avaliacao da listagem adequada dos tutores apos ordenacao.";
+		
+		this.sistema.configurarOrdem("nome");
+		
+		String listagemEsperada = "2 - Nome 2 - 2 - adress2@email.com";
+		assertEquals(msg, listagemEsperada, this.sistema.listarTutores());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConfigurarOrdemInvalida() {
+		String msg = "Avaliacao da excecao lancada ao tentar configurar uma ordem invalida.";
+		
+		this.sistema.configurarOrdem("idade");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConfigurarOrdemVazia() {
+		String msg = "Avaliacao da excecao lancada ao tentar configurar ordem a partir de uma String vazia.";
+		
+		this.sistema.configurarOrdem("");
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testConfigurarOrdemNula() {
+		String msg = "Avaliacao da excecao lancada ao tentar configurar ordem a partir de um null.";
+		
+		this.sistema.configurarOrdem(null);
+	}
+	
 }
